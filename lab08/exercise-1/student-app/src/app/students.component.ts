1cd3d1c6fff35540a82fd26f41bae3d1889d3882
Bruno Ramirez <bramirez206@hotmail.com>
2024-03-31 23:52:31 -0400

    lab06/08

---
+++ lab08/exercise-1/student-app/src/app/students.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "students",
+    template: ` <h2>{{ getTitles() }}</h2> `,
+})
+    
+export class StudentsComponent {
+    title = "List of Students";
+
+    getTitles() {
+        return this.title;
+    }
+}
